cc194fec6db8dc9361a956508e007e58244af162
Rohit - Tinitiate <krohitmahendra@gmail.com>
2024-03-28 16:42:27 +0530

    updated

--- software-installations/powerbi/install.ps1
+++ software-installations/powerbi/install.ps1
@@ -6,13 +6,16 @@ if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
+# Specify the version of Power BI Desktop to install
+$version = "2.127.1080.0"
+
 # Check if Power BI is installed by looking for the executable
 $powerBiPath = Get-Command "PBIDesktop.exe" -ErrorAction SilentlyContinue
 
 if ($powerBiPath -eq $null) {
-    # If Power BI is not installed, install it using Chocolatey
-    Write-Host "Power BI is not installed. Installing Power BI..."
-    choco install powerbi -y
+    # If Power BI is not installed, install the specified version using Chocolatey
+    Write-Host "Power BI is not installed. Installing Power BI Desktop version $version..."
+    choco install powerbi --version $version -y
 } else {
     # If Power BI is installed, skip the installation
     Write-Host "Power BI is already installed. Skipping installation."
