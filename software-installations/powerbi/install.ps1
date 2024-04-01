5cfe5b823bdc890f1df13aaefda669c02bb21d54
Rohit - Tinitiate <krohitmahendra@gmail.com>
2024-04-01 08:21:56 +0530

    updated-01-04-2024

--- software-installations/powerbi/install.ps1
+++ software-installations/powerbi/install.ps1
@@ -1,19 +1,10 @@
-# Check if Chocolatey is installed
-if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
-    Write-Host "Chocolatey is not installed. Installing Chocolatey..."
-    Set-ExecutionPolicy Bypass -Scope Process -Force
-    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-} else {
-    Write-Host "Chocolatey is already installed."
-}
+# Check if Power BI Desktop is installed
+$powerbi = Get-Package -Name "Microsoft Power BI Desktop" -ErrorAction SilentlyContinue
 
-# Check if PowerBI is installed
-$powerbiInstalled = choco list --local-only | Select-String "powerbi"
-
-if (-not $powerbiInstalled) {
-    Write-Host "PowerBI is not installed. Installing PowerBI..."
+# Install Power BI Desktop using Chocolatey if not installed
+if ($powerbi -eq $null) {
+    Write-Host "Power BI Desktop is not installed. Installing..."
     choco install powerbi-desktop -y
 } else {
-    Write-Host "PowerBI is already installed."
+    Write-Host "Power BI Desktop is already installed."
 }
