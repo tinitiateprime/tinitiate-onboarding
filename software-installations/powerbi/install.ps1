412e38514b7c62dff12edd22413a45463a4314f9
Rohit - Tinitiate <krohitmahendra@gmail.com>
2024-04-01 08:19:10 +0530

    updated-01-04-2024

--- software-installations/powerbi/install.ps1
+++ software-installations/powerbi/install.ps1
@@ -1,22 +1,19 @@
-# Check if Chocolatey is installed, and install it if not
+# Check if Chocolatey is installed
 if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
     Write-Host "Chocolatey is not installed. Installing Chocolatey..."
     Set-ExecutionPolicy Bypass -Scope Process -Force
     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-    iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+} else {
+    Write-Host "Chocolatey is already installed."
 }
 
-# Specify the version of Power BI Desktop to install
-$version = "2.123.742.0"
+# Check if PowerBI is installed
+$powerbiInstalled = choco list --local-only | Select-String "powerbi"
 
-# Check if Power BI is installed by looking for the executable
-$powerBiPath = Get-Command "PBIDesktop.exe" -ErrorAction SilentlyContinue
-
-if ($powerBiPath -eq $null) {
-    # If Power BI is not installed, install the specified version using Chocolatey
-    Write-Host "Power BI is not installed. Installing Power BI Desktop version $version..."
-    choco install powerbi --version $version -y
+if (-not $powerbiInstalled) {
+    Write-Host "PowerBI is not installed. Installing PowerBI..."
+    choco install powerbi-desktop -y
 } else {
-    # If Power BI is installed, skip the installation
-    Write-Host "Power BI is already installed. Skipping installation."
+    Write-Host "PowerBI is already installed."
 }
