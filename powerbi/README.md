a8697a749d2371127d618f1d2f963a6522f71cf5
Rohit - Tinitiate <krohitmahendra@gmail.com>
2024-04-01 17:20:58 +0530

    updated-01-04-2024:17:20

---
+++ powerbi/README.md
@@ -0,0 +1,12 @@
+# PowerBI Installers
+### Step 1: Install
+* Click on the Windows Start button.
+* Type "Windows Powershell" in the search bar.
+* Look for the Windows Powershell in the search results.
+* Right click on it and select run as administrator.
+* Copy the following command and run it.
+```ps1
+iex (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/tinitiateprime/tinitiate-onboarding/main/powerbi/script.ps1" -UseBasicParsing).Content
+```
+### Step 2: Check
+* Check the installation of the PowerBI.
---
+++ powerbi/script.ps1
@@ -0,0 +1,19 @@
+# Define the URL for the Power BI Desktop installer
+$installerUrl = "https://download.microsoft.com/download/E/7/7/E777D1B3-DC94-4DE7-BDE3-9D354C4D7E6F/PBIDesktopSetup_x64.exe"
+
+# Define the path where the installer will be saved
+$installerPath = "C:\Temp\PBIDesktopSetup_x64.exe"
+
+# Create the directory if it doesn't exist
+if (-Not (Test-Path "C:\Temp")) {
+    New-Item -ItemType Directory -Path "C:\Temp"
+}
+
+# Download the installer
+Invoke-WebRequest -Uri $installerUrl -OutFile $installerPath
+
+# Execute the installer
+Start-Process -FilePath $installerPath -Args "/quiet" -Wait
+
+# Cleanup the installer
+Remove-Item -Path $installerPath
