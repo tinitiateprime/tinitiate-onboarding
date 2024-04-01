a0c7f0e73c23175eb83749ea0fb5184e51b65369
Rohit - Tinitiate <krohitmahendra@gmail.com>
2024-04-01 08:28:36 +0530

    updated-01-04-2024

--- software-installations/powerbi/install.ps1
+++ software-installations/powerbi/install.ps1
@@ -4,7 +4,7 @@ $powerbi = Get-Package -Name "Microsoft Power BI Desktop" -ErrorAction SilentlyC
 # Install Power BI Desktop using Chocolatey if not installed
 if ($powerbi -eq $null) {
     Write-Host "Power BI Desktop is not installed. Installing..."
-    choco install powerbi -y
+    choco install powerbi --version 2.127.1080.0 -y
 } else {
     Write-Host "Power BI Desktop is already installed."
 }
