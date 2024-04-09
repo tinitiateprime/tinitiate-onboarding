149d1c15b66129b5a27c9b31cbfccd66daf1fc9d
Rohit - Tinitiate <krohitmahendra@gmail.com>
2024-04-09 19:18:44 +0530

    09-04-2024:18:55

--- software-installers/windows/powershell.ps1
+++ software-installers/windows/powershell.ps1
@@ -1,3 +1,18 @@
+# Check the current execution policy
+$currentPolicy = Get-ExecutionPolicy
+
+# List of policies in order of permissiveness
+$policies = @('Unrestricted', 'RemoteSigned', 'AllSigned', 'Default', 'Restricted')
+
+# Compare the current policy with 'RemoteSigned'
+if ($policies.IndexOf($currentPolicy) -lt $policies.IndexOf('RemoteSigned')) {
+    # Set the execution policy to 'RemoteSigned'
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+    Write-Host "Execution policy set to 'RemoteSigned'."
+} else {
+    Write-Host "Current execution policy is '$currentPolicy'. No change needed."
+}
+
 # Check if PowerShell is installed
 if (-not (Get-Command pwsh -ErrorAction SilentlyContinue)) {
     Write-Host "PowerShell is not installed. Installing..."
